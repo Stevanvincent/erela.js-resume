36698163630a14420f1d18882f9edecdbd3537c2
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-09-01 08:12:54 +0700

    Update Node.js

--- dist/structures/Node.js
+++ dist/structures/Node.js
@@ -110,20 +110,36 @@ class Node {
     }
     /** Connects to the Node. */
     connect() {
-        if (this.connected)
-            return;
-        const headers = {
-            Authorization: this.options.password,
-            "Num-Shards": String(this.manager.options.shards),
-            "User-Id": this.manager.options.clientId,
-            "Client-Name": this.manager.options.clientName,
-            "Resume-Key": this.options.resumeKey,
-        };
-        this.socket = new ws_1.default(`ws${this.options.secure ? "s" : ""}://${this.options.host}:${this.options.port}/`, { headers });
-        this.socket.on("open", this.open.bind(this));
-        this.socket.on("close", this.close.bind(this));
-        this.socket.on("message", this.message.bind(this));
-        this.socket.on("error", this.error.bind(this));
+        if (this.options.resumeKey) {
+            if (this.connected)
+                return;
+            const headers = {
+                Authorization: this.options.password,
+                "Num-Shards": String(this.manager.options.shards),
+                "User-Id": this.manager.options.clientId,
+                "Client-Name": this.manager.options.clientName,
+                "Resume-Key": this.options.resumeKey,
+            };
+            this.socket = new ws_1.default(`ws${this.options.secure ? "s" : ""}://${this.options.host}:${this.options.port}/`, { headers });
+            this.socket.on("open", this.open.bind(this));
+            this.socket.on("close", this.close.bind(this));
+            this.socket.on("message", this.message.bind(this));
+            this.socket.on("error", this.error.bind(this));
+        } else {
+            if (this.connected)
+                return;
+            const headers = {
+                Authorization: this.options.password,
+                "Num-Shards": String(this.manager.options.shards),
+                "User-Id": this.manager.options.clientId,
+                "Client-Name": this.manager.options.clientName,
+            };
+            this.socket = new ws_1.default(`ws${this.options.secure ? "s" : ""}://${this.options.host}:${this.options.port}/`, { headers });
+            this.socket.on("open", this.open.bind(this));
+            this.socket.on("close", this.close.bind(this));
+            this.socket.on("message", this.message.bind(this));
+            this.socket.on("error", this.error.bind(this));
+        }
     }
     /** Destroys the Node and all players connected with it. */
     destroy() {
