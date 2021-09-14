7dbc53777bd13230cc96ed225aac688e4ca3f995
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-09-14 18:57:36 +0700

    Update Manager.js

--- dist/structures/Manager.js
+++ dist/structures/Manager.js
@@ -148,7 +148,7 @@ class Manager extends events_1.EventEmitter {
                 soundcloud: "sc",
                 youtube: "yt",
             };
-            const source = sources[(_a = query.source) !== null && _a !== void 0 ? _a : "soundcloud"];
+            const source = sources[(_a = query.source) !== null && _a !== void 0 ? _a : "youtube"];
             let search = query.query || query;
             if (!/^https?:\/\//.test(search)) {
                 search = `${source}search:${search}`;
